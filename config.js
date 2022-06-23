3295d46bbe2254078d29c593b1d898867cef0589
GoodBoyNeon <neonfavor160@gmail.com>
2022-06-23 17:48:51 +0545

    ✨ prettierrc and .vscode files

---
+++ config.js
@@ -0,0 +1,10 @@
+export const config = {
+	prefix: "-",
+	testGuildId: "981764311405977611",
+	clientId: "933839241962729562",
+	registerSlashcommandGlobally: true,
+	clientSettings: {
+		clientId: "933839241962729562",
+		clientSecret: "-4NbBViUFulPwEyOnBkxT-NnP0Tc-EGX",
+	},
+};
