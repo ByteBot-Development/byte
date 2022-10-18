33b61a96362bd074b120674e655614afed342862
GoodBoyNeon <goodboyneon03@gmail.com>
2022-10-18 19:30:31 +0545

    Update ping command

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,9 +1,21 @@
+import { EmbedBuilder } from 'discord.js';
 import { Command } from '../../structures/Command';
 
 export default new Command({
   name: 'ping',
   description: 'Pong!',
-  run: async ({ interaction }) => {
-    await interaction.followUp('Pong!');
+  run: async ({ interaction, client }) => {
+    const websocketPing = client.ws.ping
+    
+    const embed = new EmbedBuilder()
+      .setTitle('Byte\'s ping information')
+      .setColor('Green')
+      .setDescription(`Byte's enjoying ${websocketPing}ms ping!`)
+
+    await interaction.followUp({
+      embeds: [
+        embed
+      ]
+    })
   },
 });
